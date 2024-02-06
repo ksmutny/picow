c07c4def233e808d3c6d9bb8db33d353902333ed
Karel Smutný <karel.smutny@outlook.com>
2024-02-06 13:08:06 +0100

    Moved editor.rs to its folder as mod.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use picow::editor::{content::EditorContent, Editor};
 
 fn main() -> std::io::Result<()> {
-    let file_content = fs::read_to_string("src/editor.rs")?;
+    let file_content = fs::read_to_string("src/editor/mod.rs")?;
     let content = EditorContent::parse(&file_content);
     Editor::new(content).run()
 }
