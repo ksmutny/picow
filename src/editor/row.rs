d68a6b5dbc460157925cb18f230ba821b0122991
Karel Smutný <karel.smutny@outlook.com>
2024-06-27 20:55:13 +0200

    Indexing Row respects Unicode characters

--- src/editor/row.rs
+++ src/editor/row.rs
@@ -1,23 +1,48 @@
 use std::ops::{Index, Range, RangeFrom, RangeFull, RangeTo};
 
+use unicode_segmentation::UnicodeSegmentation;
+
 #[derive(Clone)]
 #[derive(PartialEq)]
 #[derive(Debug)]
 pub struct Row {
-    bytes: String
+    bytes: String,
+    byte_idx: Vec<usize>,
 }
 
 impl Row {
     pub fn new(str: &str) -> Self {
-        Self { bytes: str.to_string() }
+        let byte_idx = Self::idx(str);
+
+        Self {
+            bytes: str.to_string(),
+            byte_idx,
+        }
+    }
+
+    fn idx(str: &str) -> Vec<usize> {
+        let mut byte_idx = Vec::new();
+
+        for (idx, _) in str.grapheme_indices(true) {
+            byte_idx.push(idx);
+        }
+
+        byte_idx
+    }
+
+    fn byte_idx(&self, char_idx: usize) -> usize {
+        if char_idx < self.byte_idx.len() {
+            return self.byte_idx[char_idx]
+        }
+        return self.bytes.len();
     }
 
     pub fn len(&self) -> usize {
-        self.bytes.len()
+        self.byte_idx.len()
     }
 
     pub fn split_at(&self, at: usize) -> (Row, Row) {
-        let (left, right) = self.bytes.split_at(at);
+        let (left, right) = self.bytes.split_at(self.byte_idx(at));
         (Row::new(left), Row::new(right))
     }
 
@@ -32,7 +57,9 @@ impl Index<Range<usize>> for Row {
     type Output = str;
 
     fn index(&self, range: Range<usize>) -> &Self::Output {
-        &self.bytes[range.start..range.end]
+        let start = self.byte_idx(range.start);
+        let end = self.byte_idx(range.end);
+        &self.bytes[start..end]
     }
 }
 
@@ -40,7 +67,8 @@ impl Index<RangeFrom<usize>> for Row {
     type Output = str;
 
     fn index(&self, range: RangeFrom<usize>) -> &Self::Output {
-        &self.bytes[range.start..]
+        let start = self.byte_idx(range.start);
+        &self.bytes[start..]
     }
 }
 
@@ -48,7 +76,8 @@ impl Index<RangeTo<usize>> for Row {
     type Output = str;
 
     fn index(&self, range: RangeTo<usize>) -> &Self::Output {
-        &self.bytes[..range.end]
+        let end = self.byte_idx(range.end);
+        &self.bytes[..end]
     }
 }
 
@@ -58,4 +87,31 @@ impl Index<RangeFull> for Row {
     fn index(&self, _: RangeFull) -> &Self::Output {
         &self.bytes[..]
     }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn row(str: &str) -> Row {
+        Row::new(str)
+    }
+
+    macro_rules! test {
+        ($name:ident: $left:expr => $right:expr) => {
+            #[test]
+            fn $name() {
+                assert_eq!($left, $right)
+            }
+        };
+    }
+
+    test! { len: row("Příliš").len() => 6 }
+    test! { split_at: row("Příliš").split_at(3) => (row("Pří"), row("liš")) }
+    test! { concat: row("žlu").concat(&row("ťoučký")) => row("žluťoučký") }
+    test! { range: &row("žluťoučký")[1..5] => "luťo" }
+    test! { range_from: &row("žluťoučký")[2..] => "uťoučký" }
+    test! { range_to: &row("žluťoučký")[..7] => "žluťouč" }
+    test! { range_full: &row("žluťoučký")[..] => "žluťoučký" }
 }
