843c30d445032eda8476dc05f81f1b41ac5dd701
Karel Smutný <karel.smutny@outlook.com>
2024-07-08 15:14:52 +0200

    Opening file from args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 use picow::app;
+use std::{env, io::Result, path::Path};
 
-fn main() -> std::io::Result<()> {
-    app::start("src/editor/mod.rs")
+fn main() -> Result<()> {
+    match env::args().nth(1) {
+        Some(ref file_path) => if exists(file_path) {
+            app::start(file_path)
+        } else {
+            print_file_not_exists(file_path)
+        }
+        None => print_usage()
+    }
+}
+
+fn exists(file_path: &str) -> bool {
+    Path::new(&file_path).exists()
+}
+
+fn print_usage() -> Result<()> {
+    print_ok("Usage: picow <file_path>")
+}
+
+fn print_file_not_exists(file_path: &str) -> Result<()> {
+    print_ok(&format!("File {} does not exist", file_path))
+}
+
+fn print_ok(msg: &str) -> Result<()> {
+    println!("{}", msg);
+    Ok(())
 }
