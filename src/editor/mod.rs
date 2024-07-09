22a42d5f27d72bd5c4a8f94647cdd96bff5d5dac
Karel Smutný <karel.smutny@outlook.com>
2024-07-09 20:09:52 +0200

    Organized imports in editor module

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -1,31 +1,29 @@
+pub mod macros;
+
 pub mod clipboard;
 pub mod content;
 pub mod cursor;
 pub mod edit;
-pub mod state;
 pub mod events;
-pub mod viewport;
 pub mod pos;
 pub mod renderer;
 pub mod row;
-pub mod macros;
+pub mod state;
+pub mod viewport;
 
 use std::io;
 
 use crate::terminal::{events::{Event::Key, KeyCode::Esc}, reader::read_event};
 
-use events::process_event;
-use state::EditorState;
 
-
-pub fn event_loop(state: &mut EditorState) -> io::Result<()> {
+pub fn event_loop(state: &mut state::EditorState) -> io::Result<()> {
     let mut rerender_content = true;
     loop {
         renderer::render(&state, rerender_content)?;
 
         match read_event()? {
             Key(Esc, 0) => break Ok(()),
-            event => rerender_content = process_event(&event, state)
+            event => rerender_content = events::process_event(&event, state)
         }
     }
 }
